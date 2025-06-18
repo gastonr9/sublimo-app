9ba2182e7369bdb5b216bb0b75686974bba30115
Gaston Rodriguez <lpmgaston@gmail.com>
2025-06-18 14:54:43 -0300

    fix: Actualizar rutas y configuración de enrutamiento en la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Route, Routes } from "react-router";
 import "./index.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -6,12 +5,14 @@ import Generador from "./pages/Generador";
 import Articulos from "./pages/Articulos";
 import Burgon from "./pages/Burgon";
 import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/sublimo-app" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/generador" element={<Generador />} />
         <Route path="/articulos" element={<Articulos />} />
         <Route path="/burgon" element={<Burgon />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import "./styles/index.css";
+import { HashRouter } from "react-router-dom";
 import { OrderProvider } from "./context/OrderContext";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <OrderProvider>
         <App />
       </OrderProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
